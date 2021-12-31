9c663ff225442e02193483c3f879146514747d97
Marko Filipović <mrkfilipovic3@gmail.com>
2021-12-31 09:38:09 +0100

    convert image string names to numbers

--- assets/scripts/images.js
+++ assets/scripts/images.js
@@ -1,133 +1,178 @@
-var locations = [{
-	id: 1,
-	name: "Banska kosa, vrh Kamenjak (KT HPO 1.10.)",
-}, {
-	id: 2,
-	name: "Dugi otok, vrh Orljak (KT HPO 16.3.)",
-}, {
-	id: 3,
-	name: "Fratar - vrh (KT HPO 9.10.)",
-}, {
-	id: 4,
-	name: "Gromovača – vrh (KT HPO 11.6.)",
-}, {
-	id: 5,
-	name: "Hvar, vrh Sv. Nikola (KT HPO 16.6.)",
-}, {
-	id: 6,
-	name: "Kalnik - vrh Vranilac (KT HPO 3.2.)",
-}, {
-	id: 7,
-	name: "Kamenjak - vrh (KT HPO 9.12.)",
-}, {
-	id: 8,
-	name: "Kremen - vrh (KT HPO 14.4.)",
-}, {
-	id: 9,
-	name: "Lipa - vrh (KT HPO 4.2.)",
-}, {
-	id: 10,
-	name: "Ljubičko brdo – vrh (KT HPO 12.7.)",
-}, {
-	id: 11,
-	name: "Martinščak – vrh (KT HPO 7.3.)",
-}, {
-	id: 12,
-	name: "Medviđak – vrh (KT HPO 8.13.)",
-}, {
-	id: 13,
-	name: "Moslavačka gora, vrh Vis (KT HPO 2.1.)",
-}, {
-	id: 14,
-	name: "Okić – vrh (KT HPO 5.1.)",
-}, {
-	id: 15,
-	name: "Pag, vrh Sveti Vid (KT HPO 15.5.)",
-}, {
-	id: 16,
-	name: "Pelješac, vrh Sv. Ilija (KT HPO 20.1.)",
-}, {
-	id: 17,
-	name: "Poštak - vrh (KT HPO 14.3.)",
-}, {
-	id: 18,
-	name: "Risnjak - vrh (KT HPO 9.6.)",
-}, {
-	id: 19,
-	name: "Samarske stijene - vrh (KT HPO 8.6.)",
-}, {
-	id: 20,
-	name: "Snježnik - vrh (KT HPO 9.7.)",
-}, {
-	id: 21,
-	name: "Sv. Jure - vrh (KT HPO 19.2.)",
-}, {
-	id: 22,
-	name: "Sveto brdo - vrh (KT HPO 13.7.)",
-}, {
-	id: 23,
-	name: "Ugljan, vrh Šćah (KT HPO 16.1.)",
-}, {
-	id: 24,
-	name: "Vaganski vrh (KT HPO 13.6.)",
-}, {
-	id: 25,
-	name: "Veliki (Zavižanski) Pivčevac (KT HPO 11.4.)",
-}, {
-	id: 26,
-	name: "Veliki Sadikovac - vrh (KT HPO 13.1.)",
-}, {
-	id: 27,
-	name: "Veliki Zavižan - vrh (KT HPO 11.2.)",
-}, {
-	id: 28,
-	name: "Vis, vrh Hum (KT HPO 16.7.)",
-}, {
-	id: 29,
-	name: "Vlaški grad - vrh (KT HPO 13.8.)",
-}, {
-	id: 30,
-	name: "vrh Babina gomila (KT HPO 20.3.)",
-}, {
-	id: 31,
-	name: "vrh Bat (KT HPO 17.5.)",
-}, {
-	id: 32,
-	name: "vrh Bojin kuk (KT HPO 13.5.)",
-}, {
-	id: 33,
-	name: "vrh Čardak (KT HPO 1.7.)",
-}, {
-	id: 34,
-	name: "vrh Čavnovka (KT HPO 17.2.)",
-}, {
-	id: 35,
-	name: "vrh Češljakovački vis (KT HPO 1.4.)",
-}, {
-	id: 36,
-	name: "vrh Dinara (Sinjal) - KT HPO 17.1.",
-}, {
-	id: 37,
-	name: "vrh Ivačka glava (KT HPO 1.3.)",
-}, {
-	id: 38,
-	name: "vrh Ivanščica (KT HPO 3.4.)",
-}, {
-	id: 39,
-	name: "vrh Kapovac (KT HPO 1.1.)",
-}, {
-	id: 40,
-	name: "vrh Sokolić (KT HPO 19.9.)",
-}, {
-	id: 41,
-	name: "vrh Veliki Kabel (KT HPO 18.5.)",
-}, {
-	id: 42,
-	name: "vrh Vojak (KT HPO 10.6.)",
-}, {
-	id: 43,
-	name: "vrh Žbevnica (KT HPO 10.1.)",
-}, {
-	id: 44,
-	name: "Zir - vrh (KT HPO 17.1.)",
-}];
+var locations = [
+  {
+    "id": "0",
+    "name": "Banska kosa, vrh Kamenjak (KT HPO 1.10.)"
+  },
+  {
+    "id": "1",
+    "name": "Dugi otok, vrh Orljak (KT HPO 16.3.)"
+  },
+  {
+    "id": "2",
+    "name": "Fratar - vrh (KT HPO 9.10.)"
+  },
+  {
+    "id": "3",
+    "name": "Gromovača – vrh (KT HPO 11.6.)"
+  },
+  {
+    "id": "4",
+    "name": "Hvar, vrh Sv. Nikola (KT HPO 16.6.)"
+  },
+  {
+    "id": "5",
+    "name": "Kalnik - vrh Vranilac (KT HPO 3.2.)"
+  },
+  {
+    "id": "6",
+    "name": "Kamenjak - vrh (KT HPO 9.12.)"
+  },
+  {
+    "id": "7",
+    "name": "Kremen - vrh (KT HPO 14.4.)"
+  },
+  {
+    "id": "8",
+    "name": "Lipa - vrh (KT HPO 4.2.)"
+  },
+  {
+    "id": "9",
+    "name": "Ljubičko brdo – vrh (KT HPO 12.7.)"
+  },
+  {
+    "id": "10",
+    "name": "Martinščak – vrh (KT HPO 7.3.)"
+  },
+  {
+    "id": "11",
+    "name": "Medviđak – vrh (KT HPO 8.13.)"
+  },
+  {
+    "id": "12",
+    "name": "Moslavačka gora, vrh Vis (KT HPO 2.1.)"
+  },
+  {
+    "id": "13",
+    "name": "Okić – vrh (KT HPO 5.1.)"
+  },
+  {
+    "id": "14",
+    "name": "Pag, vrh Sveti Vid (KT HPO 15.5.)"
+  },
+  {
+    "id": "15",
+    "name": "Pelješac, vrh Sv. Ilija (KT HPO 20.1.)"
+  },
+  {
+    "id": "16",
+    "name": "Poštak - vrh (KT HPO 14.3.)"
+  },
+  {
+    "id": "17",
+    "name": "Risnjak - vrh (KT HPO 9.6.)"
+  },
+  {
+    "id": "18",
+    "name": "Samarske stijene - vrh (KT HPO 8.6.)"
+  },
+  {
+    "id": "19",
+    "name": "Snježnik - vrh (KT HPO 9.7.)"
+  },
+  {
+    "id": "20",
+    "name": "Sv. Jure - vrh (KT HPO 19.2.)"
+  },
+  {
+    "id": "21",
+    "name": "Sveto brdo - vrh (KT HPO 13.7.)"
+  },
+  {
+    "id": "22",
+    "name": "Ugljan, vrh Šćah (KT HPO 16.1.)"
+  },
+  {
+    "id": "23",
+    "name": "Vaganski vrh (KT HPO 13.6.)"
+  },
+  {
+    "id": "24",
+    "name": "Veliki (Zavižanski) Pivčevac (KT HPO 11.4.)"
+  },
+  {
+    "id": "25",
+    "name": "Veliki Sadikovac - vrh (KT HPO 13.1.)"
+  },
+  {
+    "id": "26",
+    "name": "Veliki Zavižan - vrh (KT HPO 11.2.)"
+  },
+  {
+    "id": "27",
+    "name": "Vis, vrh Hum (KT HPO 16.7.)"
+  },
+  {
+    "id": "28",
+    "name": "Vlaški grad - vrh (KT HPO 13.8.)"
+  },
+  {
+    "id": "29",
+    "name": "Zir - vrh (KT HPO 17.1.)"
+  },
+  {
+    "id": "30",
+    "name": "vrh Babina gomila (KT HPO 20.3.)"
+  },
+  {
+    "id": "31",
+    "name": "vrh Bat (KT HPO 17.5.)"
+  },
+  {
+    "id": "32",
+    "name": "vrh Bojin kuk (KT HPO 13.5.)"
+  },
+  {
+    "id": "33",
+    "name": "vrh Dinara (Sinjal) - KT HPO 17.1."
+  },
+  {
+    "id": "34",
+    "name": "vrh Ivanščica (KT HPO 3.4.)"
+  },
+  {
+    "id": "35",
+    "name": "vrh Ivačka glava (KT HPO 1.3.)"
+  },
+  {
+    "id": "36",
+    "name": "vrh Kapovac (KT HPO 1.1.)"
+  },
+  {
+    "id": "37",
+    "name": "vrh Sokolić (KT HPO 19.9.)"
+  },
+  {
+    "id": "38",
+    "name": "vrh Veliki Kabel (KT HPO 18.5.)"
+  },
+  {
+    "id": "39",
+    "name": "vrh Vojak (KT HPO 10.6.)"
+  },
+  {
+    "id": "40",
+    "name": "vrh Čardak (KT HPO 1.7.)"
+  },
+  {
+    "id": "41",
+    "name": "vrh Čavnovka (KT HPO 17.2.)"
+  },
+  {
+    "id": "42",
+    "name": "vrh Češljakovački vis (KT HPO 1.4.)"
+  },
+  {
+    "id": "43",
+    "name": "vrh Žbevnica (KT HPO 10.1.)"
+  }
+];
